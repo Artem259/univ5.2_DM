9131788f0eadb4995d3e6c3f1ba028f04d3ff08e
Artem Poliakov <artem4250@gmail.com>
2025-02-22 22:52:09 +0200

    update: DecisionTreeClassifier.py, main.ipynb

--- lab1/classification/DecisionTreeClassifier.py
+++ lab1/classification/DecisionTreeClassifier.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.multiclass import type_of_target
 from sklearn.utils.validation import validate_data, check_is_fitted
@@ -20,7 +21,11 @@ class DecisionTreeClassifier(ClassifierMixin, BaseEstimator):
             raise ValueError(f"Unknown label type: {type_of_target(y)}")
         self.classes_, y = np.unique(y, return_inverse=True)
 
-        # TODO
+        self.num_features_ = X.shape[1]
+        feat_indices = list(range(self.num_features_))
+        df = pd.DataFrame(X, columns=feat_indices)
+        df['y'] = y
+        self.tree_ = self._id3_algorithm(df, set(feat_indices))
 
         return self
 
@@ -30,3 +35,55 @@ class DecisionTreeClassifier(ClassifierMixin, BaseEstimator):
 
         # TODO
         return ...  # self.classes_[np.argmax(decision_scores, axis=1)]
+
+    def _id3_algorithm(self, df: pd.DataFrame, feat_indices: set[int]) -> "DecisionTreeNode":
+        tree = DecisionTreeNode()
+
+        y_counts = df['y'].value_counts()
+        most_frequent_y = y_counts.index[0]
+        if len(y_counts) == 1 or not feat_indices:
+            tree.make_leaf(most_frequent_y)
+            return tree
+
+        best_feat = max(feat_indices, key=lambda x: self._information_gain(df, x))
+        tree.make_splitter(best_feat)
+        for best_feat_value, df_group in df.groupby(by=best_feat):
+            if len(df_group) == 0:
+                child = DecisionTreeNode()
+                child.make_leaf(most_frequent_y)
+            else:
+                child = self._id3_algorithm(df_group, feat_indices - {best_feat})
+            tree.add_splitter_child(best_feat_value, child)
+
+        return tree
+
+    def _information_gain(self, df: pd.DataFrame, feat_index: int) -> float:
+        # TODO
+        return 0.5
+
+
+class DecisionTreeNode:
+    def __init__(self):
+        self.is_leaf = None
+
+        # For splitter node
+        self.feat_index = None
+        self.children = None
+
+        # For leaf node
+        self.label = None
+
+    def make_splitter(self, feat_index: int):
+        self.is_leaf = False
+        self.feat_index = feat_index
+        self.children = {}
+        self.label = None
+
+    def make_leaf(self, label: int):
+        self.is_leaf = True
+        self.feat_index = None
+        self.children = None
+        self.label = label
+
+    def add_splitter_child(self, feat_value, child_node: "DecisionTreeNode"):
+        self.children[feat_value] = child_node
