650dc49d64b950a3b74e85aa4ed4eb4fa9662c1b
Artem Poliakov <artem4250@gmail.com>
2025-02-23 02:51:48 +0200

    update: DecisionTreeClassifier.py, main.ipynb

--- lab1/classification/DecisionTreeClassifier.py
+++ lab1/classification/DecisionTreeClassifier.py
@@ -25,6 +25,8 @@ class DecisionTreeClassifier(ClassifierMixin, BaseEstimator):
         feat_indices = list(range(self.num_features_))
         df = pd.DataFrame(X, columns=feat_indices)
         df['y'] = y
+
+        self.feature_unique_values_ = [set(df[feat_index]) for feat_index in feat_indices]
         self.tree_ = self._id3_algorithm(df, set(feat_indices))
 
         return self
@@ -33,8 +35,15 @@ class DecisionTreeClassifier(ClassifierMixin, BaseEstimator):
         check_is_fitted(self)
         X = validate_data(self, X, reset=False)
 
-        # TODO
-        return ...  # self.classes_[np.argmax(decision_scores, axis=1)]
+        y_pred = []
+        for x in X:
+            curr_node = self.tree_
+            while not curr_node.is_leaf:
+                feat_value = x[curr_node.feat_index]
+                curr_node = curr_node.children[feat_value]  # TODO handle the unknown value
+            y_pred.append(curr_node.label)
+
+        return self.classes_[y_pred]
 
     def _id3_algorithm(self, df: pd.DataFrame, feat_indices: set[int]) -> "DecisionTreeNode":
         node = DecisionTreeNode()
@@ -47,12 +56,17 @@ class DecisionTreeClassifier(ClassifierMixin, BaseEstimator):
 
         best_feat = max(feat_indices, key=lambda x: self._information_gain(df, x))
         node.make_splitter(best_feat)
+
         for best_feat_value, df_group in df.groupby(by=best_feat):
-            if len(df_group) == 0:
-                child_node = DecisionTreeNode()
-                child_node.make_leaf(most_frequent_y)
-            else:
-                child_node = self._id3_algorithm(df_group, feat_indices - {best_feat})
+            child_node = self._id3_algorithm(df_group, feat_indices - {best_feat})
+            node.add_splitter_child(best_feat_value, child_node)
+
+        all_best_feat_values = self.feature_unique_values_[best_feat]
+        df_best_feat_values = set(df[best_feat])
+        unseen_best_feat_values = all_best_feat_values - df_best_feat_values
+        for best_feat_value in unseen_best_feat_values:
+            child_node = DecisionTreeNode()
+            child_node.make_leaf(most_frequent_y)
             node.add_splitter_child(best_feat_value, child_node)
 
         return node
