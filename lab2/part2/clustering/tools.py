c109adacd6454df3a2955a809d6918062f5cb14c
Artem Poliakov <artem4250@gmail.com>
2025-03-03 22:44:35 +0200

    add(part2): tools.py

---
+++ lab2/part2/clustering/tools.py
@@ -0,0 +1,13 @@
+import math
+
+
+def euclidean_distance(p1, p2):
+    return math.sqrt(sum((a - b) ** 2 for a, b in zip(p1, p2)))
+
+
+def calc_zip_distances(set1, set2, distance_metric):
+    return [distance_metric(p1, p2) for p1, p2 in zip(set1, set2)]
+
+
+def calc_max_zip_distance(set1, set2, distance_metric):
+    return max(calc_zip_distances(set1, set2, distance_metric))
