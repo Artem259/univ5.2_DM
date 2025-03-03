9cb88c5b8a0ba846a4a46745b16887081411764d
Artem Poliakov <artem4250@gmail.com>
2025-03-03 22:02:19 +0200

    update(part2): KMeans.py, main.ipynb

--- lab2/part2/clustering/KMeans.py
+++ lab2/part2/clustering/KMeans.py
@@ -4,10 +4,15 @@ from sklearn.utils.validation import validate_data
 
 
 class KMeans(ClusterMixin, BaseEstimator):
-    def __init__(self):
+    def __init__(self, n_clusters, init, max_iter=300, e=1e-4):
         super().__init__()
+        self.n_clusters = n_clusters
+        self.init = init
+        self.max_iter = max_iter
+        self.e = e
 
     def fit(self, X, y=None):
+        self.__validate_params()
         X = validate_data(self, X)
         X = np.array(X)
 
@@ -15,3 +20,27 @@ class KMeans(ClusterMixin, BaseEstimator):
 
         self.labels_ = ...
         return self
+
+    def __validate_params(self):
+        if not isinstance(self.n_clusters, int) or self.n_clusters < 1:
+            raise ValueError(
+                f"The 'n_clusters' parameter of KMeans must be an int in the range [1, inf). "
+                f"Got '{self.n_clusters}' instead."
+            )
+        if not isinstance(self.max_iter, int) or self.max_iter < 1:
+            raise ValueError(
+                f"The 'max_iter' parameter of KMeans must be an int in the range [1, inf). "
+                f"Got '{self.max_iter}' instead."
+            )
+        if not (isinstance(self.e, float) or isinstance(self.e, int)) or self.e < 0:
+            raise ValueError(
+                f"The 'e' parameter of KMeans must be a float in the range [0, inf). "
+                f"Got '{self.e}' instead."
+            )
+
+        init_shape = np.array(self.init).shape
+        if init_shape[0] != self.n_clusters:
+            raise ValueError(
+                f"The shape of the initial centers {init_shape} "
+                f"does not match the number of clusters {self.n_clusters}."
+            )
