675a9f6dce1ee78a8cf1ae9b9fb8d106f7b5d4ee
Artem Poliakov <artem4250@gmail.com>
2025-03-05 00:42:02 +0200

    update(part2): tools.py, KMeans.py

--- lab2/part2/clustering/KMeans.py
+++ lab2/part2/clustering/KMeans.py
@@ -1,24 +1,28 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClusterMixin
+from sklearn.utils import check_random_state
 from sklearn.utils.validation import validate_data
 
 from . import tools
 
 
 class KMeans(ClusterMixin, BaseEstimator):
-    def __init__(self, n_clusters, init, max_iter=300, e=1e-4):
+    def __init__(self, n_clusters=8, init='random', max_iter=300, e=1e-4, random_state=None):
         super().__init__()
         self.n_clusters = n_clusters
         self.init = init
         self.max_iter = max_iter
         self.e = e
+        self.random_state = random_state
 
     def fit(self, X, y=None):
         self.__validate_params()
+        self.random_state_ = check_random_state(self.random_state)
         X = validate_data(self, X)
         X = np.array(X)
 
-        self.cluster_centers_ = np.array(self.init)
+        self._init_fit(X)
+        self.cluster_centers_ = self._init_cluster_centers(X)
         self.labels_ = self._recalc_labels(X)
         self.n_iter_ = 0
 
@@ -33,6 +37,15 @@ class KMeans(ClusterMixin, BaseEstimator):
                 break
 
         return self
+
+    def _init_fit(self, X):
+        pass
+
+    def _init_cluster_centers(self, X):
+        if isinstance(self.init, str) and self.init == 'random':
+            random_indices = self.random_state_.choice(X.shape[0], self.n_clusters, replace=False)
+            return np.array(X[random_indices])
+        return np.array(self.init)
 
     def _recalc_cluster_centers(self, X):
         return np.array([
@@ -50,28 +63,35 @@ class KMeans(ClusterMixin, BaseEstimator):
             old_cluster_centers,
             tools.euclidean_distance
         )
-        return max_centers_dist_diff < self.e
+        return max_centers_dist_diff <= self.e
 
     def __validate_params(self):
         if not isinstance(self.n_clusters, int) or self.n_clusters < 1:
             raise ValueError(
-                f"The 'n_clusters' parameter of KMeans must be an int in the range [1, inf). "
+                f"The 'n_clusters' parameter must be an int in the range [1, inf). "
                 f"Got '{self.n_clusters}' instead."
             )
         if not isinstance(self.max_iter, int) or self.max_iter < 1:
             raise ValueError(
-                f"The 'max_iter' parameter of KMeans must be an int in the range [1, inf). "
+                f"The 'max_iter' parameter must be an int in the range [1, inf). "
                 f"Got '{self.max_iter}' instead."
             )
         if not (isinstance(self.e, float) or isinstance(self.e, int)) or self.e < 0:
             raise ValueError(
-                f"The 'e' parameter of KMeans must be a float in the range [0, inf). "
+                f"The 'e' parameter must be a float in the range [0, inf). "
                 f"Got '{self.e}' instead."
             )
 
-        init_shape = np.array(self.init).shape
-        if init_shape[0] != self.n_clusters:
-            raise ValueError(
-                f"The shape of the initial centers {init_shape} "
-                f"does not match the number of clusters {self.n_clusters}."
-            )
+        if isinstance(self.init, str):
+            if self.init not in ('random', ):
+                raise ValueError(
+                    f"The 'init' parameter must be array-like or a str among ['random']. "
+                    f"Got '{self.init}' instead."
+                )
+        else:
+            init_shape = np.array(self.init).shape
+            if not init_shape or init_shape[0] != self.n_clusters:
+                raise ValueError(
+                    f"The shape of the initial centers {init_shape} "
+                    f"does not match the number of clusters {self.n_clusters}."
+                )
--- lab2/part2/clustering/tools.py
+++ lab2/part2/clustering/tools.py
@@ -1,20 +1,31 @@
 import math
+import numpy as np
 
 
 def euclidean_distance(p1, p2):
     return math.sqrt(sum((a - b) ** 2 for a, b in zip(p1, p2)))
 
 
-def calc_zip_distances(set1, set2, distance_metric):
+def calc_zip_distances(set1, set2, distance_metric=euclidean_distance):
     return [distance_metric(p1, p2) for p1, p2 in zip(set1, set2)]
 
 
-def calc_max_zip_distance(set1, set2, distance_metric):
+def calc_max_zip_distance(set1, set2, distance_metric=euclidean_distance):
     return max(calc_zip_distances(set1, set2, distance_metric))
 
 
-def calc_distance_matrix(points, centers, distance_metric):
-    return [
-        [distance_metric(point, center) for center in centers]
+def calc_min_zip_distance(set1, set2, distance_metric=euclidean_distance):
+    return min(calc_zip_distances(set1, set2, distance_metric))
+
+
+def calc_distance_matrix(points, targets, distance_metric=euclidean_distance):
+    return np.array([
+        [distance_metric(point, target) for target in targets]
         for point in points
-    ]
+    ])
+
+
+def find_closest_point(points, target, distance_metric=euclidean_distance):
+    distances = calc_zip_distances(points, [target] * len(points), distance_metric)
+    closest_index = int(np.argmin(distances))
+    return closest_index, points[closest_index]
